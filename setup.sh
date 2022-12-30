7543baf1fa34b817039dd7e4547c75081120539e
dchung117 <dchung0330@gmail.com>
2022-12-30 03:43:27 +0000

    added lines to install python 3.10 and mysql.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Update apt
 sudo apt update
+sudo apt upgrade
 
 # Install docker
 if ! [ -x "$(command -v docker)" ];
@@ -36,4 +37,21 @@ then curl -sS https://webinstall.dev/k9s | bash
 else echo "K9s already installed."
 fi
 
-# todo: install/upgrade python to 3.10, install mysql
+# Install python3.10
+sudo apt install python3.10
+#OLD_VERSION="$(python3 --version | grep '^Python 3\.')"
+#echo $OLD_VERSION
+
+#IFS="."
+#read -ra arr <<< "$OLD_VERSION"
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.${arr[1]} 1 # add old version to alternative list
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2 # add new version
+#sudo update-alternatives --config python3
+#sudo rm /usr/bin/python3 # remove old symlink
+#sudo ln -s python 3.10 /usr/bin/python3 # update symlink to python3.10
+python3 --version
+# Install MySQL
+if ! [ -x "$(command -v mysql)" ]
+then sudo snap install mysql-shell
+else echo "MySQL already installed."
+fi
